d0765a87d5d13be4dafe4659f29d0647f1fabc3c
kliost <yra.shkolniy@gmail.com>
2023-06-16 23:40:53 +0300

    paging is complete. cart design is not full-complete. admin panel needs better design.

--- Ensemble Elegance/Controllers/HomeController.cs
+++ Ensemble Elegance/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Ensemble_Elegance.Models;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Ensemble_Elegance.Controllers
 {
@@ -16,10 +18,24 @@ namespace Ensemble_Elegance.Controllers
             return View();
         }
 
-        public async Task<IActionResult> Catalogue()
+        public async Task<IActionResult> Catalogue(int page = 1)
         {
+            int pagesize = 25;
+
             var items = await _context.ShopItems.ToListAsync();
-            return View(items);
+
+            int totalItems = items.Count();
+            int totalPages = (int)Math.Ceiling(totalItems / (double)pagesize);
+            var paginatedData = items.Skip((page - 1) * pagesize).Take(pagesize).ToList();
+
+            var paginationModel = new PaginationModel
+            {
+                CurrentPage = page,
+                PageSize = pagesize,
+                PageCount = totalPages,
+            };
+            ViewData["paginationModel"] = paginationModel;
+            return View(paginatedData);
         }
 
         public IActionResult UserPage()
---
+++ Ensemble Elegance/Models/PaginationModel.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Identity.Client;
+using System.Net;
+
+namespace Ensemble_Elegance.Models
+{
+    public class PaginationModel
+    {
+        public int CurrentPage { get; set; }
+        public int PageSize { get; set; }
+        public int PageCount { get; set; }
+    }
+}
